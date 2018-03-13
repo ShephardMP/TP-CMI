edd4fdd01a2b8ba9fcc0a7249968574bcc3b1bf1
Tomasmed18 <tomy-medina@hotmail.com>
2018-03-13 19:31:03 -0300

    Agrego la opción de elegir la cantidad de clusters
    En la ventana selección se puede elegir cuantos clusters se van a hacer. Le mandé que pueda elegir entre 2 y 12, y con ese numero configura el ClusterGenerator.

--- VentanaSeleccion.py
+++ VentanaSeleccion.py
@@ -49,31 +49,32 @@ def destroy_VentanaSeleccion():
 class VentanaSeleccion:
     #genera una interfaz con indicadores y opciones y devuelve las opciones elegidas en mapaElecciones, se debe proporcionar el mapa de labels y las opciones
     #El maximo que soporta son 5 labels, further work is requiered.
-    
-    admin = None
+
+    adminInter = None
     mapElecciones=None
     resultados=None
     top=None
     def cerrar(self):
         self.top.destroy()
-        
+
     def aceptarOpciones(self):
         for K in self.mapElecciones:
                 self.resultados[K]=self.mapElecciones[K].get() #mapElecciones en la misma clave que es un texto debiera ser un combobox con una opcion seleccionada
-        self.admin.mostrarCluster()
+        self.adminInter.mostrarCluster(cantClusters = int(self.comboCantClusters.get()))
         self.cerrar()
+
     def insertarAccionesBotones(self):
         self.aceptar.configure(command=self.aceptarOpciones)
         self.cancelar.configure(command=self.cerrar)
-        
+
     def __init__(self, top=None,AdminInterfaz = None, mapaLabelsXContenido=None, resultados=None):
         '''This class configures and populates the toplevel window.
            top is the toplevel containing window.'''
         _bgcolor = '#d9d9d9'  # X11 color: 'gray85'
         _fgcolor = '#000000'  # X11 color: 'black'
         _compcolor = '#d9d9d9' # X11 color: 'gray85'
-        _ana1color = '#d9d9d9' # X11 color: 'gray85' 
-        _ana2color = '#d9d9d9' # X11 color: 'gray85' 
+        _ana1color = '#d9d9d9' # X11 color: 'gray85'
+        _ana2color = '#d9d9d9' # X11 color: 'gray85'
         self.style = ttk.Style()
         if sys.platform == "win32":
             self.style.theme_use('winnative')
@@ -88,14 +89,14 @@ class VentanaSeleccion:
         top.configure(background="#d9d9d9")
         top.configure(highlightbackground="#d9d9d9")
         top.configure(highlightcolor="black")
-        
-        
+
+
         Xlabel=0.02
         Ylabel=0.28
-        
+
         Xbox=0.02
         Ybox=0.5
-        
+
         self.aceptar = Button(top)
         self.aceptar.place(relx=Xbox, rely=0.78, height=44, width=117)
         self.aceptar.configure(activebackground="#d9d9d9")
@@ -121,11 +122,20 @@ class VentanaSeleccion:
         self.cancelar.configure(pady="0")
         self.cancelar.configure(text='''Cancelar''')
 
+        self.labCantClusters = ttk.Label(top, text="Cantidad de clusters")
+        self.labCantClusters.place(relx= Xbox+0.4, rely=0.78)
+        self.labCantClusters.configure(background="#d9d9d9")
+
+        self.comboCantClusters = ttk.Combobox(top, state="readonly", width=15 )
+        self.comboCantClusters.place(relx= Xbox+0.4, rely=0.88)
+        self.comboCantClusters["values"] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        self.comboCantClusters.set(2)
+
         self.mapElecciones={} #label x combobox
         self.resultados=resultados
         self.top=top
-        self.admin = AdminInterfaz
-        
+        self.adminInter = AdminInterfaz
+
         if(mapaLabelsXContenido is not None):
             for K in mapaLabelsXContenido:
                     Label1 = Label(top)
@@ -134,24 +144,24 @@ class VentanaSeleccion:
                     Label1.configure(disabledforeground="#a3a3a3")
                     Label1.configure(foreground="#000000")
                     Label1.configure(text=K)
-                     
+
                     TCombobox1 = ttk.Combobox(top,state='readonly')
                     TCombobox1.place(relx=Xbox, rely=Ybox, relheight=0.12, relwidth=0.17)
-            
+
                     TCombobox1.configure(textvariable=StringVar()) #Si uso el combobox de ventanaSeleccion_support se comparte el mismo combobox para todos los valores
                     TCombobox1.configure(takefocus="")
 
                     TCombobox1["values"]=mapaLabelsXContenido[K]
                     #TCombobox1.bind("<<ComboboxSelected>>", self.selection_changed)  EVENTO
-                    
+
                     self.mapElecciones[K]=TCombobox1
                     Xlabel=Xlabel+0.2
                     Xbox=Xbox+0.2
 
-            
+
 
         self.insertarAccionesBotones()
-        
+
 
 
 if __name__ == '__main__':
