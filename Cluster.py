cc094287b71ff72c3be203ee139cc60bbd0829c5
ShephardMP <35865469+ShephardMP@users.noreply.github.com>
2018-05-03 09:28:53 -0300

    Add files via upload
    Cluster ahora es una clase que tiene datos pasados por un algoritmo de clustering. Tiene etiquetas de lo que se hizo clustering y demas cosas (como la distribucion de los puntos por cluster y el numero de clusters).
    
    ClusterGenerator ahora genera cluster (!!!!!1!!!!!!11). Ya no retorna figuras.
    
    Ventana Ploteo ahora se encarga de todo lo referente a lo del ploteo, recibe un cluster y extrae la informacion necesaria para realizar el grafico acorde. Tambien se encarga de lo de los colores.
    
    Ya probe un par de casos y andaba, despues igual pruebo que este todo coherente (colores con respecto a cluster)

--- Cluster.py
+++ Cluster.py
@@ -4,25 +4,47 @@ Created on Fri Apr 27 19:28:37 2018
 
 @author: Mauro
 """
-
+import numpy as np
 class Cluster:
-    figura=None #de tipo matplotlib.figure
+    
+    #el cluster tiene nada mas que sus datos clusterisados, etiquetas y como se distribuyen los puntos en base el algoritmo de clustering
+  
     diccionarioClustersYPuntos=None 
-    diccionarioClustersYColores=None
-    
+    labelX=None
+    labelY=None
+    data=None
     coefCorr=None
-    #es un diccionario que tiene como clave el label del cluster 
-    #(ej: cluster A) y la cantidad de puntos que tiene
-    def __init__(self, plot,coefCorr,diccLabelsXCantidadPuntos=None,diccLabelsXColores=None):
-        self.diccionarioClustersYColores={}
+    distribucion=None
+    cantClusters=None
+   
+    def __init__(self, datos,labelX=None,labelY=None,distribucion=None,numClusters=None):
+        self.data=datos #ya cambiados en base a una tecnica de clustering
+        self.cantClusters=numClusters
+        
         self.diccionarioClustersYPuntos={}
-        self.figura=plot #esta figura se mostraria haciendo self.plot = FigureCanvasTkAgg(self.figura, master=top) en una interfaz
-        self.diccionarioClustersYPuntos=diccLabelsXCantidadPuntos
-        self.diccionarioClustersYColores=diccLabelsXColores
-        self.coefCorr=coefCorr
-    def getFigura(self):
-        return self.figura
+        
+        puntosClusters={i: np.where(distribucion == i)[0] for i in range(numClusters)} 
+        self.diccionarioClustersYPuntos={i: sum(1 for x in puntosClusters[i]) for i in puntosClusters.keys()} 
+        
+      
+        #lo anterior crea un diccionar con el numero de cluster como clave y todos los puntos como contenido
+        #i es clave y el np.where es una lista por comprension
+        #lo siguiente es un diccionario con los cluster y los puntos que tienen
+        
+        self.distribucion=distribucion
+        
+        correlacion=np.corrcoef(datos[:, 0],datos[:, 1])
+        correlacionXY=float("{0:.5f}".format(correlacion[0,1])) #correlacion es una matriz de todo x con todo y, tiene 1 en la diagonal
+        self.coefCorr=correlacionXY
+        
+        self.labelX=labelX
+        self.labelY=labelY
+         #self.figura=plot #esta figura se mostraria haciendo self.plot = FigureCanvasTkAgg(self.figura, master=top) en una interfaz
+    def getData(self):
+        return self.data
     
+ 
+
     
     def getCantPuntos(self,nombreCluster):
         return self.diccionarioClustersYPuntos[nombreCluster]
@@ -34,7 +56,18 @@ class Cluster:
     def getClustersYPuntos(self):
         return self.diccionarioClustersYPuntos
     
-    def getClustersYColores(self):
-        return self.diccionarioClustersYColores
+    def getCantClusters(self):
+        return self.cantClusters
+
+    def getDistribucion(self):
+        return self.distribucion
+    
+    
     def getCorrelacion(self):
         return self.coefCorr
+    
+    def getEtiquetaX(self):
+        return self.labelX
+    
+    def getEtiquetaY(self):
+        return self.labelY
